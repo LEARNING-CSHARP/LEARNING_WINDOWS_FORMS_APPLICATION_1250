af8a5f8f2c061767b17af868af5a5b2bd9477087
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-05-10 12:18:22 +0430

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,35 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		int minute;
+		int second;
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+			minute = 0;
+			second = 0;
+		}
+
+		private void titleTimer_Tick(object sender, System.EventArgs e)
+		{
+			//int minute = 0;
+			//int second = 0;
+
+			titleLabel.Text = $"{ minute } : { second }";
+
+			second++;
+
+			if (second > 60)
+			{
+				second = 0;
+				minute++;
+			}
+		}
+	}
+}
